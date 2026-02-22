af33704d83e9c90c708b64c8768a75bcd9b87256
Hyeli Ko <76466882+highlyko17@users.noreply.github.com>
2026-02-22 16:02:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.19 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.13 MB, 시간: 0.27 ms
+메모리: 4.14 MB, 시간: 0.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 02일 19:02:07
+2026년 02월 22일 16:02:57
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,44 +1,40 @@
-#include <vector>
-#include <queue>
+#include<vector>
+#include<queue>
+
 using namespace std;
 
-int dx[4] = {1, -1, 0, 0};
-int dy[4] = {0, 0, 1, -1};
+
 
 int solution(vector<vector<int> > maps)
 {
-    int n = maps.size(); //세로, i, x
-    int m = maps[0].size(); //가로, j, y
+    int n = maps.size(); int m = maps[0].size();
     
-    //if(maps[n - 2][m - 1] == 0 && maps[n - 1][m - 2] == 0) return -1;
+    int dn[4] = {1, -1, 0, 0};
+    int dm[4] = {0, 0, 1, -1};
     
     queue<pair<int, pair<int, int>>> q;
-    vector<vector<bool>> visited(n, vector<bool>(m, false));
     q.push({1, {0, 0}});
-    visited[0][0] = true;
     
     while(!q.empty()){
-        int cnt = q.front().first;
-        int x = q.front().second.first;
-        int y = q.front().second.second;
+        int dist = q.front().first;
+        int cn = q.front().second.first;
+        int cm = q.front().second.second;
         q.pop();
         
-        if(x == n - 1 && y == m - 1){
-            return cnt;
+        if(cn == n - 1 && cm == m - 1){
+            return dist;
         }
         
         for(int i = 0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            int nn = cn + dn[i];
+            int nm = cm + dm[i];
             
-            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+            if(nn < 0 || nm < 0 || nn >= n || nm >= m || !maps[nn][nm]) continue;
             
-            if(maps[nx][ny] == 1 && !visited[nx][ny]){
-                q.push({cnt + 1, {nx, ny}});
-                visited[nx][ny] = true;
-            }
+            q.push({dist + 1, {nn, nm}});
+            maps[nn][nm] = 0;
         }
-    }
+    }  
     
     return -1;
 }
