983d45b3bc169608b821014f7d23df6720ad82ba
Hyeli Ko <76466882+highlyko17@users.noreply.github.com>
2026-06-20 18:40:40 +0900

    [level 2] Title: 타겟 넘버, Time: 35.90 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=cpp) 
 
 ### 성능 요약
 
-메모리: 3.67 MB, 시간: 69.44 ms
+메모리: 3.94 MB, 시간: 35.90 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 03일 11:43:13
+2026년 06월 20일 18:40:36
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 int answer = 0;
 
-void dfs(vector<int> numbers, int target, int sum, int idx){
+void dfs(vector<int> numbers, int target, int idx, int sum){
     if(idx == numbers.size()){
         if(sum == target) answer++;
         return;
     }
     
-    dfs(numbers, target, sum + numbers[idx], idx + 1);
-    dfs(numbers, target, sum - numbers[idx], idx + 1);
+    dfs(numbers, target, idx + 1, sum + numbers[idx]);
+    dfs(numbers, target, idx + 1, sum - numbers[idx]);
 }
 
-int solution(vector<int> numbers, int target) {
+
+int solution(vector<int> numbers, int target) {    
     dfs(numbers, target, 0, 0);
     
     return answer;
